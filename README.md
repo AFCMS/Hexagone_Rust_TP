6fbfb6a77b1f8daf46e28d9616e0e97532a5e063
AFCMS <afcm.contact@gmail.com>
2025-11-27 15:09:47 +0100

    More work

---
+++ README.md
@@ -0,0 +1,29 @@
+# Hexagone - TP Rust
+
+Deux struct principales: `Livre` et `Bibliotheque`.
+
+```mermaid
+classDiagram
+    class Livre {
+        +String titre
+        +String auteur
+        +u16 annee
+        +bool disponible
+    }
+    class Bibliotheque {
+        +Vec<Livre> livres
+        +ajouter_livre(livre: Livre)
+        +emprunter_livre(titre: &str) -> Option<&Livre>
+        +retourner_livre(titre: &str) -> Option<&Livre>
+        +contient_livre(titre: &str) -> bool
+        +livres_disponibles() -> Vec<&Livre>
+    }
+```
+
+La logique d'affichage et d'input utilisateur est volontairement séparée dans des fonctions dédiées dans un souci de responsabilité unique pour les struct crées.
+
+Pour simplifier la construction de `Bibliotheque` et éviter de passer un tableau vide à chaque fois, on dérive le trait `Default` pour cette struct.
+
+L'affichage des livres se fait simplement en dérivant le trait `Debug` pour la struct `Livre`.
+
+Pour le fun, j'ai ajouté un mini pipeline GitHub Actions qui compile le projet à chaque push Git, et j'ai ajouté plusieurs optimisations de compilation dans les fichiers `Cargo.toml` et `.cargo/config.toml` (ciblage de l'architecture CPU native, optimisation plus aggressive, etc).
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(dead_code)]
+// Dead code requis pour les chanps auteur et annee de Livre qui ne sont utilisés
+// que via le trait Debug (pour affichage) et causent donc des warnings compilateur
 
 use std::io;
 
@@ -10,15 +12,113 @@ struct Livre {
     disponible: bool,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Bibliotheque {
     livres: Vec<Livre>,
 }
 
-fn main() {
-    println!("Hello, world!");
+impl Bibliotheque {
+    fn ajouter_livre(&mut self, livre: Livre) {
+        self.livres.push(livre);
+    }
+    fn emprunter_livre(&mut self, titre: &str) -> Option<&Livre> {
+        self.livres
+            .iter_mut()
+            .find(|livre| livre.titre == titre && livre.disponible)
+            .map(|livre| {
+                livre.disponible = false;
+                &*livre
+            })
+    }
+    fn retourner_livre(&mut self, titre: &str) -> Option<&Livre> {
+        self.livres
+            .iter_mut()
+            .find(|livre| livre.titre == titre && !livre.disponible)
+            .map(|livre| {
+                livre.disponible = true;
+                &*livre
+            })
+    }
+    fn contient_livre(&self, titre: &str) -> bool {
+        self.livres.iter().any(|livre| livre.titre == titre)
+    }
+    /**
+     * Renvoie une liste de pointeurs vers les livres disponibles
+     */
+    fn livres_disponibles(&self) -> Vec<&Livre> {
+        self.livres
+            .iter()
+            .filter(|livre| livre.disponible)
+            .collect()
+    }
+}
 
-    let mut bib = Bibliotheque { livres: Vec::new() };
+const FAILED_STDIN_READ: &str = "Impossible de lire stdin";
+
+fn prompt_ajouter_livre(bib: &mut Bibliotheque) {
+    let mut titre = String::new();
+    let mut auteur = String::new();
+    let mut annee_str = String::new();
+
+    println!("Titre: ");
+    io::stdin().read_line(&mut titre).expect(FAILED_STDIN_READ);
+
+    if bib.contient_livre(titre.trim()) {
+        println!("Le livre '{}' existe déjà", titre.trim());
+        return;
+    }
+
+    println!("Auteur: ");
+    io::stdin().read_line(&mut auteur).expect(FAILED_STDIN_READ);
+
+    println!("Année: ");
+    io::stdin()
+        .read_line(&mut annee_str)
+        .expect(FAILED_STDIN_READ);
+
+    let annee: u16 = annee_str
+        .trim()
+        .parse()
+        .expect("L'année doit etre un nombre");
+
+    let nouveau_livre = Livre {
+        titre: titre.trim().to_string(),
+        auteur: auteur.trim().to_string(),
+        annee,
+        disponible: true,
+    };
+
+    bib.ajouter_livre(nouveau_livre);
+
+    println!("Livre ajouté");
+}
+
+fn prompt_emprunter_livre(bib: &mut Bibliotheque) {
+    let mut titre = String::new();
+
+    println!("Titre: ");
+    io::stdin().read_line(&mut titre).expect(FAILED_STDIN_READ);
+
+    match bib.emprunter_livre(titre.trim()) {
+        Some(livre) => println!("Emprunté: {:#?}", livre),
+        None => println!("Livre non disponible ou introuvable"),
+    }
+}
+
+fn prompt_retourner_livre(bib: &mut Bibliotheque) {
+    let mut titre = String::new();
+
+    println!("Titre: ");
+    io::stdin().read_line(&mut titre).expect(FAILED_STDIN_READ);
+
+    match bib.retourner_livre(titre.trim()) {
+        Some(livre) => println!("Vous avez retourné: {:#?}", livre),
+        None => println!("Livre introuvable ou déjà disponible"),
+    }
+}
+
+fn main() {
+    let mut bib = Bibliotheque::default();
 
     bib.livres.push(Livre {
         titre: String::from("Le Petit Prince"),
@@ -27,8 +127,6 @@ fn main() {
         disponible: true,
     });
 
-    println!("{:#?}", bib);
-
     println!("======Bibliothèque======");
     println!("1. Ajouter un livre");
     println!("2. Emprunter un livre");
@@ -36,27 +134,45 @@ fn main() {
     println!("4. Afficher tous les livres");
     println!("5. Afficher les livres disponibles");
     println!("6. Quitter");
-    println!("Votre choix:");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
 
-    // trim pour enlever les espaces et convertir de String en str
-    match input.trim() {
-        "1" => println!("Ajouter un livre"),
-        "4" => {
-            for livre in &bib.livres {
-                println!("{:#?}", livre);
+    loop {
+        println!("\nVotre choix:");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect(FAILED_STDIN_READ);
+
+        // trim pour enlever les espaces et convertir de String en str
+        match input.trim() {
+            "1" => {
+                prompt_ajouter_livre(&mut bib);
             }
-        }
-        "5" => {
-            for livre in &bib.livres {
-                if livre.disponible {
-                    println!("{:#?}", livre);
+            "2" => {
+                prompt_emprunter_livre(&mut bib);
+            }
+            "3" => {
+                prompt_retourner_livre(&mut bib);
+            }
+            "4" => {
+                if bib.livres.is_empty() {
+                    println!("Aucun livres");
+                    continue;
                 }
+
+                bib.livres.iter().for_each(|livre| println!("{:#?}", livre));
             }
+            "5" => {
+                let dispos = bib.livres_disponibles();
+                if dispos.is_empty() {
+                    println!("Aucun livres");
+                    continue;
+                }
+
+                dispos.iter().for_each(|livre| println!("{:#?}", livre));
+            }
+            "6" => {
+                break;
+            }
+            _ => println!("Option invalide"),
         }
-        _ => println!("Option non implémentée"),
     }
 }
